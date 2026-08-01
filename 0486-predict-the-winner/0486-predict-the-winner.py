ca037805f0f99c4bcae25965eef3e5daf030a432
Aryan Saxena <iamcodezero1@gmail.com>
2026-08-01 14:35:31 +0530

    Time: 4 ms (39.38%), Space: 19.9 MB (27.38%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        @lru_cache(None)
+        def dfs(left, right):
+            if left == right:
+                return nums[left]
+
+            take_left = nums[left] - dfs(left + 1, right)
+            take_right = nums[right] - dfs(left, right - 1)
+
+            return max(take_left, take_right)
+
+        return dfs(0, len(nums) - 1) >= 0
